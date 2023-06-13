3d905788df4c0f65310073ac8bad9c4c13e43bc6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-13 17:01:52 -0600

    add reverseString function

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ function sum (a,b){
 
 /*    ---- Start Task 1 ----    */ 
 function stringLength(string){
-    console.log('runing stringLength')
-    console.log('string to check =',string)
     length = string.length;
     //code bellow
     if(length >= 1 && length <= 10) {
@@ -15,15 +13,28 @@ function stringLength(string){
     }
 
     //code upwards
-    console.log(result);
     return result;
 };
 /*    ---- Ends Task 1 ----    */ 
 /*    ---- Start Task 2 ----    */ 
+function reverseString(string){
+        //convertimos a array para poder manipular
+        let strFromArr = [...string];
+        let result = [];
+
+        strFromArr.forEach(character => {
+            result.unshift(character);
+            console.log('result after loop',result)
+        });
+
+        result = result.join('');
+
+        return result;
+}
 /*    ---- Ends Task 2 ----    */ 
 /*    ---- Start Task 3 ----    */ 
 /*    ---- Ends Task 3 ----    */ 
 /*    ---- Start Task 4 ----    */ 
 /*    ---- Ends Task 4 ----    */ 
 
-module.exports = { stringLength, sum }
+module.exports = { stringLength, sum, reverseString }

75c1c488d46af9fad87a2badbe1ec143b41d9884
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-13 16:12:26 -0600

    add function stringLength to count chara in a string

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ function sum (a,b){
     return a+b;
 }
 
-module.exports = sum;
+/*    ---- Start Task 1 ----    */ 
+function stringLength(string){
+    console.log('runing stringLength')
+    console.log('string to check =',string)
+    let result = string.length;
+    console.log(result);
+    return result;
+};
+/*    ---- Ends Task 1 ----    */ 
+/*    ---- Start Task 2 ----    */ 
+/*    ---- Ends Task 2 ----    */ 
+/*    ---- Start Task 3 ----    */ 
+/*    ---- Ends Task 3 ----    */ 
+/*    ---- Start Task 4 ----    */ 
+/*    ---- Ends Task 4 ----    */ 
+
+module.exports = { stringLength, sum }

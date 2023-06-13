c8071900e910254b37ed1a4cef8426386a5bcf3d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-13 16:13:23 -0600

    add test for stringLengh function, refactored import statement to import correctly all functions

--- main.test.js
+++ main.test.js
@@ -1,5 +1,11 @@
-const sum = require('./main.js')
+const { sum, stringLength } = require( './main' );
 
 it ('result of the sum', ()=>{
     expect (sum(1,2)).toBe(3)
+})
+
+// Task 1
+
+it ('stringLenght counter ', ()=>{
+    expect ( stringLength('abcd') ).toBe(4)
 })
